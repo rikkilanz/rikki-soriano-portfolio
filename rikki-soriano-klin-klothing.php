7ccad6506acee3c22448e949b998dbde3d27f1d8
Rikki Soriano <rikkilanz@gmail.com>
2022-11-22 20:42:49 -0800

    Update rikki-soriano-klin-klothing.php

--- rikki-soriano-klin-klothing.php
+++ rikki-soriano-klin-klothing.php
@@ -14,52 +14,36 @@
                 </div>
                 <div class="project__intro">
                     <h1>klin klothing | Mobile App Prototype</h1>
-                    <p>A mobile first responsive website coded with HTML, CSS and JS.</p>
+                    <p>A working Adobe XD mobile app prototype for a fictional clothing brand.</p>
                     <ul class="project__type">
-                        <li class="pill pill--almond">CODING</li>
+                        <li class="pill pill--almond">WIREFRAMING</li>
                         <li class="pill pill--almond">UIUX</li>
                     </ul>
                 </div>
                 <div class="project__info">
                     <div class="project__timeline">
                         <h4>Timeline</h4>
-                        <p>2 Weeks</p>
+                        <p>6 Weeks</p>
                     </div>
                     <div class="project__tools">
                         <h4>Tools Used</h4>
                         <ul>
                             <li>Adobe Illustrator</li>
-                            <li>Visual Studio Code</li>
-                        </ul>
-                    </div>
-                    <div class="project__languages">
-                        <h4>Languages</h4>
-                        <ul>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JS</li>
+                            <li>Adobe XD</li>
                         </ul>
                     </div>
                 </div>
                 <div class="project__process">
                     <div class="box">
-                        <p><b>The purpose</b> of the project was to create a responsive website design for mobile and desktop users. Another project requirement was to implement 3 Javascript plugins for the website (Added Slick Slider, Accordion, and Lightbox-JS)</p>
-                    </div>
-                    <div class="box">
-                        <h3>Coding Process</h3>
-                        <p>I started coding the header first to be mobile responsive. During this, I also created a hamburger menu with Javascript as a project requirement. Afterwards, I started sectioning the contents and added necessary Javascript plugins for the functionality.</p>
-                            
-                        <p>As a project bonus, we were to add a video media when the browser reaches a desktop size. This was a struggle since the video was added as a background and doesn't size responsively (which I managed to do afterwards.)</p>
+                        <p><b>The purpose</b> of the project is to create a prototype app for a fictional business with a specific user archetype and target audience in mind. I created the prototype with Adobe XD and user-tested it with UseBerry.</p>
                     </div>
                     <div class="box">
                         <h3>Wireframing</h3>
-                        <p>The content of the website is layed out to be easily responsive. All of the content is center aligned because the purpose of the landing webpage is just to show content quick and in divided sections.</p>
-                        
-                        <p>The website is layed out base on information hierarchy. I used a gallery slider to highlight appealing visuals for users to capture their attention. It's proceeded with the companys mission and values to reinforce towards the users what the company's focus is on.</p>
-                        
-                        <p>The team is introduced afterwards, highlighting what they work as and a brief description about them. In this section, I also implemented on a Lightbox Javascript (when you click an Image, it zooms in your screen) to let users have the option see the image better.</p>
-                        
-                        <p>Finally a FAQ Section using an Accordion Javascript plugin to easily show and hide information without crowding the UI.</p>
+                        <p>Before the sketching phase, I've researched and followed existing app design systems from other retail companies (H&M, Uniqlo, Shein). Most of the apps had a lot of images to highlight their products and use some sort of a gallery slider for users to browse. All of them are actually contained in a card to create some unity and content division</p>
+                    </div>
+                    <div class="box">
+                        <h3>Design Process</h3>
+                        <p>The idea for the logo is to stick with the business branding of being a minimal clothing brand. I played around with a bunch of typefaces from serif to sans-serifs and ended up using Crimson Pro. The type's thick serifs and serious tone fits the brand really well of presenting minimalism with an organic and authentic feel.</p>
                     </div>
                     <div class="box">
                         <img src="images/rikki-soriano-terra-fortuna-logo-process.jpg" alt="">
