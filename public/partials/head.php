0f38606fcf8b36a0b776855c0e06c5467ed4f2e7
Rikki Soriano <rikkilanz@gmail.com>
2023-02-23 14:37:26 -0800

    Update head.php

--- public/partials/head.php
+++ public/partials/head.php
@@ -1,5 +1,5 @@
 <?php
-    $page_title = $page_title ?? 'Rikki Soriano, Front-End Developer & UIUX Designer | Portfolio';
+    $page_title = $page_title ?? 'Rikki Soriano, Front-End Developer & UX Designer | Portfolio';
     $page_desc = $page_desc ?? 'Hello! My name is Rikki Soriano, a Designer/Front-End Developer based in Vancouver, Canada. I invite you to come check my design and development works in this portfolio!';
 ?>
 <head>
@@ -14,6 +14,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title?></title>
+    <meta property="og:title" content="<?php echo $page_title?>" />
+    <meta property="og:url" content="https://www.rikkisoriano.com/" />
+    <meta property="og:description" content="Hello! My name is Rikki Soriano, a Designer/Front-End Developer based in Vancouver, Canada. I invite you to come check my design and development works in this portfolio!" />
+    <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
     <meta name="description" content="Hello! My name is Rikki Soriano, a Designer/Front-End Developer based in Vancouver, Canada. I invite you to come check my design and development works in this portfolio!">
     <meta name="keywords" content="rikki soriano, uiux designer, frontend developer, vancouver web developer, vancouver uiux designer, bcit new media, uiux designer, graphic designer, web developer, vancouver">
     <meta name="author" content="Rikki Soriano">
