25baf569179996ae67a98cfde5fade84ed506dcb
Rikki Soriano <rikkilanz@gmail.com>
2023-02-28 21:38:11 -0800

    Update 404.php

--- public/404.php
+++ public/404.php
@@ -15,7 +15,7 @@
                 <img src="<?php echo get_public_url('images/')?>404-shape.svg" alt="">
             </div>
             <div class="page-not-found-info">
-                <h1 class="intro__title">You must be lost!?</h1>
+                <h1 class="intro__title">You must bae lost!?</h1>
                 <div class="cta"><a class="btn btn--s" href="index.php">Quick! press this button to make everything okay :)</a></div>
             </div>
         </section>
