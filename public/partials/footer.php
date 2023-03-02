ecc20cc9caf22e50e842dd503cbf0fdb1dba761c
Rikki Soriano <rikkilanz@gmail.com>
2023-03-02 09:35:46 -0800

    test

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -28,18 +28,18 @@
 
 <?php
 
-// echo $_SERVER['REQUEST_URI'];
+echo $_SERVER['REQUEST_URI'];
 
-if(str_contains($_SERVER['REQUEST_URI'], '/projects')){
-    echo '<script type="module">
-    import jump from "../scripts/jump.module.js";
-    let topBtn = document.getElementById("topBtn");
-    topBtn.addEventListener("click", () => {
-        jump("html");
-        console.log("hello");
-    })
-</script>';
-}
+// if(str_contains($_SERVER['REQUEST_URI'], '/projects')){
+//     echo '<script type="module">
+//     import jump from "../scripts/jump.module.js";
+//     let topBtn = document.getElementById("topBtn");
+//     topBtn.addEventListener("click", () => {
+//         jump("html");
+//         console.log("hello");
+//     })
+// </script>';
+// }
 
 ?>
 
