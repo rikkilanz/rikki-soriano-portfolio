836ef67ef3688944352513e3612115f189f6bc79
Rikki Soriano <rikkilanz@gmail.com>
2023-03-02 21:38:23 -0800

    added new projects

--- src/data/projects.php
+++ src/data/projects.php
@@ -14,25 +14,36 @@
         // We assign the value string description to constructed variable 'description'
         'description' => 'A website structured with HTML, styled with CSS, and used JavaScript plugins.',
         // Here, we create an array to support multiple data
-        'type' => ['CODING']
+        'type' => ['CODING', 'HTML', 'CSS', 'JS', ]
     ]);
 
     $klin_klothing = new Project([
         'name' => 'klin klothing | Mobile App Prototype',
         'description' => 'A working Adobe XD prototype of a mobile app for a fictional clothing brand.',
-        'type' => ['UIUX', 'WIREFRAMING']
+        'type' => ['UIUX', 'WIREFRAMING', 'Adobe XD',]
     ]);
 
     $live_2k = new Project([
         'name' => 'Live 2K | Coded Website Prototype',
         'description' => 'A working website protoype coded with HTML and CSS',
-        'type' => ['UIUX', 'WIREFRAMING', 'CODING']
+        'type' => ['CODING', 'PHP', 'UIUX']
     ]);
 
     $fizzy_pop= new Project([
         'name' => 'Fizzy POP | Beverage Branding',
         'description' => 'A branding project for a fictional soda beverage company.',
-        'type' => ['GRAPHICS', 'BRANDING']
+        'type' => ['GRAPHICS', 'BRANDING', 'PACKAGING']
+    ]);
+
+    $weather_now= new Project([
+        'name' => 'Weather Now | React.js Web App',
+        'description' => 'A weather app using an API call to display the weather forecast using the React.js framework',
+        'type' => ['CODING', 'React.js']
+    ]);
+    $filcoop_bc= new Project([
+        'name' => 'FIL-COOP BC | Website Redesign',
+        'description' => 'A redesign for a cooperative website, FIL-COOP BC',
+        'type' => ['UIUX', 'Figma']
     ]);
 
     // This is the array that we call in our index.php with a foreach loop so it loops through all the objects and gets displayed in the card with their assigned values in this file.
@@ -41,4 +52,6 @@
         $klin_klothing,
         $live_2k,
         $fizzy_pop,
+        $weather_now,
+        // $filcoop_bc,
     ];
