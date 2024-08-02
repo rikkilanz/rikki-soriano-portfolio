fc7816780e24489dc8653e6852acc7b0882895dd
Rikki Soriano <rikkilanz@gmail.com>
2024-08-01 19:25:53 -0700

    initial tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
+      colors: {
+        // Use for majority of designs (backgroud color, buttons, headers)
+        "princetonorange": "#FE950B",
+        "orangepeel": "#FEA634",
+        "papayawhip": "#FFEFD5",
+        "babypowder": "#FDFFFC",
+        "black": "#0A0908",
+        "walnut": "#696047"
+      },
+      fontFamily: {
+        gibson: ["canada-type-gibson", "sans-serif"],
       },
     },
   },
