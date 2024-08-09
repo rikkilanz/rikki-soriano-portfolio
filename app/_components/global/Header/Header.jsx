ef96990a2800d935db223b426aded20f23aaf737
Rikki Soriano <rikkilanz@gmail.com>
2024-08-08 21:49:37 -0700

    Navigation bar

--- app/_components/global/Header/Header.jsx
+++ app/_components/global/Header/Header.jsx
@@ -1,17 +1,84 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
-    <header className="bg-transparent absolute bottom-0 left-0 right-0 min-h-[100px] px-8 flex z-10">
-      <div className="flex justify-between items-center flex-grow uppercase text-babypowder text-lg font-medium">
-        <nav className="flex gap-2">
-          <Link href="/work">Work</Link>
-          <Link href="/about">About</Link>
+    <header className="bg-transparent absolute bottom-0 left-0 right-0 min-h-[100px] flex z-10">
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex flex-col w-full uppercase text-lg font-medium">
+        <div className=" flex justify-between items-center flex-grow px-8 py-4">
+          <Link href="/">
+            <Image
+              src="/assets/brand-logo/rikkisoriano-logo-horizontal-white.svg"
+              height={0}
+              width={0}
+              className="h-12 w-auto"
+            />
+          </Link>
+          <button
+            className="relative"
+            onClick={() => {
+              setIsNavOpen(!isNavOpen);
+            }}
+          >
+            <Image
+              src="/assets/icons/triangle-icon.svg"
+              height={0}
+              width={0}
+              className={`${
+                isNavOpen ? "rotate-[-180deg]" : ""
+              } h-8 w-auto active:bottom-[-4px] relative transition-all duration-300 ease-in-out`}
+            />
+            <Image
+              src="/assets/icons/triangle-icon-back.svg"
+              height={0}
+              width={0}
+              className={`${
+                isNavOpen ? "rotate-[-180deg]" : ""
+              } h-8 w-auto absolute top-1 z-[-1] transition-all duration-300 ease-in-out`}
+            />
+          </button>
+        </div>
+        {isNavOpen && (
+          <div className="py-8 bg-white">
+            <nav className="flex flex-col">
+              <Link href="/work" className="hover:font-extrabold px-8 py-4">
+                Work
+              </Link>
+              <Link href="/about" className="hover:font-extrabold px-8 py-4">
+                About
+              </Link>
+            </nav>
+            <div className="px-8 py-4">
+              <Link href="/contact">
+                <div className="relative">
+                  <div className="bg-babypowder text-princetonorange px-8 py-3 rounded-md border-2 border-black font-extrabold relative active:bottom-[-6px] text-center z-[2]">
+                    Contact Me
+                  </div>
+                  <div className="bg-black absolute w-full h-full top-2 rounded-md left-0 right-0 border-4 border-black z-[1]" />
+                </div>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* Desktop Navigation */}
+      <div className="hidden sm:flex justify-between items-center flex-grow uppercase text-babypowder text-lg font-medium px-8">
+        <nav className="flex gap-4">
+          <Link href="/work" className="hover:font-extrabold">
+            Work
+          </Link>
+          <Link href="/about" className="hover:font-extrabold">
+            About
+          </Link>
         </nav>
         <Link href="/">
           <Image
-            src="/assets/brand-logo/rikki-soriano-logo-icon-white.svg"
+            src="/assets/brand-logo/rikkisoriano-logo-icon-white.svg"
             height={0}
             width={0}
             className="w-8"
@@ -19,7 +86,7 @@ export default function Header() {
         </Link>
         <Link href="/contact">
           <div className="relative">
-            <div className="bg-babypowder text-princetonorange px-8 py-3 rounded-md border-2 border-black font-extrabold relative active:bottom-[-6px]">
+            <div className="bg-babypowder text-princetonorange px-8 py-3 rounded-md border-2 border-black font-extrabold relative active:bottom-[-6px] text-center">
               Contact Me
             </div>
             <div className="bg-black absolute w-full h-full top-2 rounded-md left-0 right-0 border-4 border-black z-[-1]" />
